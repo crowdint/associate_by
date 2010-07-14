81c94410c0b17cf7a02e907be763b8d16bcbb819
David Padilla <david.padilla@crowdint.com>
2010-07-14 14:42:38 -0500

    Made tests a bit more readable (I think)

--- test/associate_by_test.rb
+++ test/associate_by_test.rb
@@ -9,12 +9,12 @@ ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":me
 
 def setup_db
   ActiveRecord::Schema.define(:version => 1) do
-    create_table :parents do |t|
+    create_table :movies do |t|
       t.column :name, :string
     end
 
-    create_table :children do |t|
-      t.column :parent_id, :integer
+    create_table :actors do |t|
+      t.column :movie_id, :integer
       t.column :name, :string
     end
   end
@@ -26,47 +26,17 @@ def teardown_db
   end
 end
 
-class Category < ActiveRecord::Base
-  has_many :products
-  associate_by :products, :name
-
-  def self.table_name 
-    "parents"
-  end
-end
-
-class Product < ActiveRecord::Base
-  associate_by :category, :name
-  belongs_to :category
-
-  def self.table_name 
-    "children"
-  end
-end
-
 class Actor < ActiveRecord::Base
-  associate_by :movie, :name, :create => true
   belongs_to :movie
-
-  def self.table_name 
-    "children"
-  end
 end
 
 class Movie < ActiveRecord::Base
-  associate_by :actors, :name, :create => true
   has_many :actors
-
-  def self.table_name 
-    "parents"
-  end
 end
 
 class AssociateByTest < Test::Unit::TestCase
   def setup
     setup_db
-    @category = Category.create(:name => 'Category')
-    @product = Product.create(:name => 'Product')
     @movie= Movie.create(:name => 'Movie')
     @actor= Actor.create(:name => 'Actor')
   end
@@ -76,38 +46,43 @@ class AssociateByTest < Test::Unit::TestCase
   end
 
   def test_has_many_association
-    @category.product_name = @product.name
-    @category.save
-    assert_equal(true, @category.products.include?(@product))
+    Movie.send(:associate_by, :actors, :name)
+    @movie.actor_name = @actor.name
+    @movie.save
+    assert_equal(true, @movie.actors.include?(@actor))
   end
 
   def test_belongs_to_association
-    @product.category_name = @category.name
-    @product.save
-    assert_equal(@category, @product.category)
+    Actor.send(:associate_by, :movie, :name)
+    @actor.movie_name = @movie.name
+    @actor.save
+    assert_equal(@movie, @actor.movie)
   end
 
   def test_create_has_many_association
+    Movie.send(:associate_by, :actors, :name, {:create => true})
     @movie.actor_name = 'A Different Actor'
     @movie.save
     assert_equal(1, Actor.where(:name => 'A Different Actor').count)
   end
 
   def test_create_belongs_to_association
+    Actor.send(:associate_by, :movie, :name, {:create => true})
     @actor.movie_name = 'A Different Movie'
     @actor.save
     assert_equal(1, Movie.where(:name => 'A Different Movie').count)
   end
-  
+
   def test_belongs_to_reader
-    @product.category = @category
-    @product.save
-    assert_equal(@category.name, @product.category_name)
+    Actor.send(:associate_by, :movie, :name, {:create => true})
+    @actor.movie = @movie
+    assert_equal(@movie.name, @actor.movie_name)
   end
-  
+
   def test_has_many_reader
-    @category.products << @product
-    assert_equal("", @category.product_name)
+    Movie.send(:associate_by, :actors, :name)
+    @movie.actors << @actor
+    assert_equal("", @movie.actor_name)
   end
 end
 
